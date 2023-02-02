abf955fb4329211bd4b98bca53822b119aed2816
hysoka1984 <122939491+hysoka1984@users.noreply.github.com>
2023-02-02 18:50:21 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cursoCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
